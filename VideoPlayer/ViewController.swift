5335b80fd2ecf9072bc3e11372f8453afa75a655
Kalpataru Sahoo <andolasoft.user105@gmail.com>
2019-07-24 16:28:39 +0530

    cmpleted video player project

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Andolasoft on 7/24/19.
+//  Copyright © 2019 Kalpataru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
